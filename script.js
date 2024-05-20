79a1052a5abd558e54e3640e034bd5250fcc3002
Lucas-FA <lucasfreitasdeabreu@hotmail.com>
2024-05-20 15:21:20 -0300

    feat: Menu hambúrguer e formulário implemnetados

---
+++ script.js
@@ -0,0 +1,54 @@
+let menu = document.getElementById("menu");
+let iconeBarras = document.getElementById("icone-barras");
+let iconeX = document.getElementById("icone-x");
+
+function abrirFecharMenu() {
+    if(menu.classList.contains("menu-fechado")) {
+        menu.classList.remove("menu-fechado");
+        iconeX.style.display = "inline";
+        iconeBarras.style.display = "none";
+    }
+    else {
+        menu.classList.add("menu-fechado");
+        iconeX.style.display = "none";
+        iconeBarras.style.display = "inline";
+    }
+}
+
+window.onresize = () => {
+    menu.classList.remove("menu-fechado");
+    iconeX.style.display = "inline";
+    iconeBarras.style.display = "none";
+}
+
+const entrarEmContato = (event) => {
+    let valorNome = document.getElementById("campo-nome").value;
+    let valorEmail = document.getElementById("campo-email").value;
+    let valorAssunto = document.getElementById("campo-assunto").value;
+
+    let dadosForm = {
+        nome: valorNome,
+        email: valorEmail,
+        assunto: valorAssunto
+    }
+
+    fetch("http://127.0.0.1:3000/solicitacoes", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dadosForm)
+    })
+    .then(resposta => {
+        console.log(resposta);
+        document.querySelector("#contato form").reset();
+        alert("Solicitação cadastrada com sucesso!");
+    })
+    .catch(erro => {
+        console.error(erro);
+        alert("Erro");
+    })
+
+    event.preventDefault();
+}
+
